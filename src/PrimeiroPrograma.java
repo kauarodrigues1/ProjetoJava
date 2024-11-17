508e06711c528fae4f7a018feacbe1729dd1f911
Kaua <rodrigueskaua76@gmail.com>
2024-11-17 19:05:25 -0300

    abrindo public static void

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        // armazenar propriedades e variaveis
+        System.out.println(("Hello world"));
+        System.out.println((200 / 2));
+    }
+
+}
